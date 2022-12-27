4b91df9d1c900b94feb27ec8664615c96e7f2a38
Nick Connolley <nconnolley@outlook.com>
2022-12-26 22:16:28 -0600

    initialized

---
+++ adventure_time/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{width: "100vw", height:"100vh", background: "#2c2c2e"}}>
+      <div style={{background: "green", display: "flex", justifyContent: "center", alignItems: "center"}}>
+        <h1 style={{width: "90vw", height: "10vh", }}>
+          This disgusting shit got me groovin man fr i cant even feel my ass its
+          numb bruh fuck this tight hole stewie fuck
+        </h1>
+      </div>
+      <img
+        src="https://yt3.googleusercontent.com/ytc/AMLnZu_A4kIcmw0ddT1izhk5F8-HVYuNU4yu0gTTPRxH=s900-c-k-c0x00ffffff-no-rj"
+        alt="bmo chop"
+        style={{ maxHeight: "20vh", maxWidth: "20vw" }}
+      />
+      <a href="https://www.facebook.com" target="_blank">
+        <img
+          src="https://i.etsystatic.com/32177011/r/il/5b7f3d/3705389954/il_fullxfull.3705389954_4lfm.jpg"
+          alt=""
+          style={{ maxHeight: "20vh", maxWidth: "20vw" }}
+        />
+      </a>
+    </div>
+  );
+}
+
+export default App;
